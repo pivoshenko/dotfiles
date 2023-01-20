f476192916374f5cd07158db7bd2ff2f70c78961
volopivoshenko <volodymyr.pivoshenko@gmail.com>
2023-01-20 19:18:28 +0000

    refactor: apply formatters

--- dotfiles/.ipython/start.py
+++ dotfiles/.ipython/start.py
@@ -1,13 +1,12 @@
-""""IPython - startup script."""
+"""IPython - startup script."""
 
 from IPython import get_ipython
 
-
 ipython = get_ipython()
 
-if "ipython" in globals():
-    ipython.editing_mode = "vi"
-    ipython.run_line_magic("load_ext", "autoreload")
-    ipython.run_line_magic("autoreload", "2")
+if "ipython" in globals():  # noqa: WPS421, Q000
+    ipython.editing_mode = "vi"  # noqa: Q000
+    ipython.run_line_magic("load_ext", "autoreload")  # noqa: Q000
+    ipython.run_line_magic("autoreload", "2")  # noqa: Q000
 
-from pprint import pprint as print
+from pprint import pprint  # noqa: F401, E402
