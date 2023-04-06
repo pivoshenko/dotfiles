04726945949ac15bd7936e30d499fc296cf5863d
volopivoshenko <volodymyr.pivoshenko@gmail.com>
2023-04-06 19:16:09 +0100

    refactor: refactor

--- dotfiles/.ipython/profile_default/startup/start.py
+++ dotfiles/.ipython/profile_default/startup/start.py
@@ -1,12 +1,12 @@
 """IPython - startup script."""
 
-from IPython import get_ipython
+from IPython import get_ipython  # type: ignore
 
 ipython = get_ipython()
 
 if "ipython" in globals():  # noqa: WPS421, Q000
-    ipython.editing_mode = "vi"  # noqa: Q000
-    ipython.run_line_magic("load_ext", "autoreload")  # noqa: Q000
-    ipython.run_line_magic("autoreload", "2")  # noqa: Q000
+    ipython.editing_mode = "vi"  # noqa: Q000  # type: ignore
+    ipython.run_line_magic("load_ext", "autoreload")  # noqa: Q000 E501  # type: ignore
+    ipython.run_line_magic("autoreload", "2")  # noqa: Q000  # type: ignore
 
 from pprint import pprint  # noqa: F401, E402
