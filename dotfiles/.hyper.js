cedde7d6f96a6f97c6e60323cbf5b57b0ae4d94f
volopivoshenko <volodymyr.pivoshenko@gmail.com>
2023-05-06 13:31:02 +0100

    chore: chore

--- dotfiles/.hyper.js
+++ dotfiles/.hyper.js
@@ -2,7 +2,8 @@ module.exports = {
     config: {
         fontFamily: "MesloLGM Nerd Font",
         fontSize: 13,
+        catppuccinTheme: "macchiato",
     },
 
-    plugins: ["hyper-one-dark", "hyper-tabs-enhanced"],
+    plugins: ["hyper-one-dark", "hyper-tabs-enhanced", "hypurr#latest", "hyper-font-smoothing"],
 };
