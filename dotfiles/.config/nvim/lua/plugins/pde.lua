e79f75d58221e845f95865b94b4e05b443502c93
Volodymyr Pivoshenko <volodymyr.pivoshenko@gmail.com>
2023-07-18 19:18:08 +0100

    feat: update `nvim`

--- dotfiles/.config/nvim/lua/plugins/pde.lua
+++ dotfiles/.config/nvim/lua/plugins/pde.lua
@@ -1,44 +1,45 @@
 return {
-  {
-    "echasnovski/mini.nvim",
-    version = false,
-    config = function()
-      require("mini.files").setup()
-    end,
-    keys = {
-      {
-        "<leader>m",
-        function()
-          require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
+    {
+        "echasnovski/mini.nvim",
+        version = false,
+        config = function()
+            require("mini.files").setup()
         end,
-        desc = "Open mini.files (directory of current file)",
-      },
-      {
-        "<leader>M",
-        function()
-          require("mini.files").open(vim.loop.cwd(), true)
-        end,
-        desc = "Open mini.files (cwd)",
-      },
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      {
-        "SmiteshP/nvim-navbuddy",
-        dependencies = {
-          "SmiteshP/nvim-navic",
-          "MunifTanjim/nui.nvim",
+        keys = {
+            {
+                "<leader>m",
+                function()
+                    require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
+                end,
+                desc = "Open mini.files (directory of current file)",
+            },
+            {
+                "<leader>M",
+                function()
+                    require("mini.files").open(vim.loop.cwd(), true)
+                end,
+                desc = "Open mini.files (cwd)",
+            },
         },
-        opts = { lsp = { auto_attach = true } },
-      },
     },
-  },
-  {
-    "NvChad/nvterm",
-    config = function()
-      require("nvterm").setup()
-    end,
-  },
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            {
+                "SmiteshP/nvim-navbuddy",
+                dependencies = {
+                    "SmiteshP/nvim-navic",
+                    "MunifTanjim/nui.nvim",
+                },
+                opts = { lsp = { auto_attach = true } },
+            },
+        },
+    },
+    {
+        "akinsho/toggleterm.nvim",
+        version = "*",
+        config = function()
+            require("toggleterm").setup()
+        end,
+    },
 }
