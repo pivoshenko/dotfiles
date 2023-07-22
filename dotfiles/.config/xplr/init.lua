897b1eebecedfa0f528825344641a8b41093bdc9
Volodymyr Pivoshenko <volodymyr.pivoshenko@gmail.com>
2023-07-22 22:02:33 +0100

    style: apply formatter

--- dotfiles/.config/xplr/init.lua
+++ dotfiles/.config/xplr/init.lua
@@ -8,22 +8,22 @@ package.path = package.path .. ";" .. xpm_path .. "/?.lua;" .. xpm_path .. "/?/i
 
 os.execute(string.format("[ -e '%s' ] || git clone '%s' '%s'", xpm_path, xpm_url, xpm_path))
 
-require("xpm").setup({'dtomvan/xpm.xplr'})
+require("xpm").setup({ 'dtomvan/xpm.xplr' })
 
-require('xpm').setup {'dtomvan/xpm.xplr', {
+require('xpm').setup { 'dtomvan/xpm.xplr', {
     'dtomvan/extra-icons.xplr',
     after = function()
         xplr.config.general.table.row.cols[2] = {
             format = "custom.icons_dtomvan_col_1"
         }
     end
-}}
+} }
 
-require('xpm').setup {'dtomvan/xpm.xplr', {
+require('xpm').setup { 'dtomvan/xpm.xplr', {
     'prncss-xyz/icons.xplr',
     after = function()
         xplr.config.general.table.row.cols[2] = {
             format = "custom.icons_dtomvan_col_1"
         }
     end
-}}
+} }
