330860f92d283d1b405506e9f7d6e92936785b24
Volodymyr Pivoshenko <volodymyr.pivoshenko@gmail.com>
2023-07-23 22:19:45 +0100

    fix: update `neovim`

--- dotfiles/.config/nvim/lua/plugins/lsp.lua
+++ dotfiles/.config/nvim/lua/plugins/lsp.lua
@@ -8,24 +8,6 @@ return {
                 sources = {
                     null_ls.builtins.formatting.lua_format,
                 },
-                on_attach = function(_, _)
-                    vim.api.nvim_create_augroup("Format on save", { clear = false })
-                    vim.api.nvim_create_autocmd("BufWritePre", {
-                        callback = function()
-                            vim.lsp.buf.format {
-                                async = true,
-                                filter = function(client)
-                                    -- if client.supports_method "textDocument/formatting" then
-                                    --   return client.name ~= "null-ls"
-                                    -- end
-                                    return client.name == "null-ls"
-                                end,
-                            }
-                            vim.notify("Format Done", vim.log.levels.INFO, { title = "Format" })
-                        end,
-                        group = "Format on save",
-                    })
-                end,
             }
         end,
     },
