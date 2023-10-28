35a0c6cc6ac55af3a709b82cc9d2d8e0d2f86e3a
Volodymyr Pivoshenko <volodymyr.pivoshenko@gmail.com>
2023-10-28 12:37:04 +0100

    feat: remove `neovim`

--- dotfiles/.config/nvim/lua/config/keymaps.lua
+++
@@ -1,5 +0,0 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-
-vim.cmd('map <Leader>lf :lua vim.lsp.buf.format({ timeout_ms = 2000 })<CR>')
