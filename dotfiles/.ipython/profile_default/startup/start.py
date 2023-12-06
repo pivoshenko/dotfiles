acc8ba06e1cfdac32107cdc01cf750f96322e867
Volodymyr Pivoshenko <volodymyr.pivoshenko@gmail.com>
2023-12-06 11:22:49 +0000

    refactor(ipython): remove unused linter comments

--- dotfiles/.ipython/profile_default/startup/start.py
+++ dotfiles/.ipython/profile_default/startup/start.py
@@ -1,12 +1,12 @@
 """IPython - startup script."""
 
-from IPython import get_ipython  # type: ignore
+from IPython import get_ipython
 
 ipython = get_ipython()
 
-if "ipython" in globals():  # noqa: WPS421, Q000
-    ipython.editing_mode = "vi"  # noqa: Q000  # type: ignore
-    ipython.run_line_magic("load_ext", "autoreload")  # noqa: Q000 E501  # type: ignore
-    ipython.run_line_magic("autoreload", "2")  # noqa: Q000  # type: ignore
+if "ipython" in globals():
+    ipython.editing_mode = "vi"
+    ipython.run_line_magic("load_ext", "autoreload")
+    ipython.run_line_magic("autoreload", "2")
 
-from pprint import pprint  # noqa: F401, E402
+from pprint import pprint as print  # noqa: F401, E402
