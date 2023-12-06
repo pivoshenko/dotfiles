d86c6a37640044cc50715951836d3ff3df32743a
Volodymyr Pivoshenko <volodymyr.pivoshenko@gmail.com>
2023-12-06 11:35:14 +0000

    feat(ipython): add config for vi editing mode

---
+++ dotfiles/.ipython/profile_default/ipython_config.py
@@ -0,0 +1,27 @@
+"""IPython - default configuration."""
+
+import sys
+
+from prompt_toolkit.key_binding.vi_state import InputMode
+from prompt_toolkit.key_binding.vi_state import ViState
+
+
+def get_input_mode(self):
+    return self._input_mode
+
+
+def set_input_mode(self, mode):
+    shape = {InputMode.NAVIGATION: 1, InputMode.REPLACE: 3}.get(mode, 5)
+    raw = u"\x1b[{} q".format(shape)
+    if hasattr(sys.stdout, "_cli"):
+        out = sys.stdout._cli.output.write_raw
+    else:
+        out = sys.stdout.write
+    out(raw)
+    sys.stdout.flush()
+    self._input_mode = mode
+
+
+ViState._input_mode = InputMode.INSERT  # type: ignore[attr-defined]
+ViState.input_mode = property(get_input_mode, set_input_mode)  # type: ignore[method-assign, assignment]
+c.TerminalInteractiveShell.editing_mode = "vi"  # type: ignore[name-defined]  # noqa: F821
