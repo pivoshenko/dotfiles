820d5c7881454db92969bb72346007db42b3e01e
pivoshenko <volodymyr.pivoshenko@gmail.com>
2025-06-14 16:26:33 +0100

    feat: regular update

--- dotfiles/.config/fish/completions/gleam.fish
+++ dotfiles/.config/fish/completions/gleam.fish
@@ -1,13 +1,12 @@
 # Fish Completions for Gleam
-# https://github.com/glpda/gleam-completions/blob/master/gleam.fish
 
-set -l commands add build check clean deps docs export fix format help hex lsp new publish remove run shell test update
+set -l commands add build check clean deps dev docs export fix format help hex lsp new publish remove run shell test update
 set -l commands_with_help deps docs export hex
 set -l targets erlang javascript
 set -l runtimes nodejs deno bun
 
 function __fish_gleam_project_root
-    # Get project root (parent dir with gleam.toml)
+    # get project root (parent dir with gleam.toml)
     for dir in (__fish_parent_directories (pwd --physical))
         if test -f $dir/gleam.toml
             echo $dir
@@ -18,7 +17,7 @@ function __fish_gleam_project_root
 end
 
 function __fish_gleam_runnable_module
-    # TODO list files under src and test with a main function
+    # TODO list files under src, test and dev with a main function
     # NOTE files under 'build/packages/*/src' can also be run
     # NOTE may handle path completion 'gleam run -m package/module'
 end
@@ -28,7 +27,7 @@ end
 
 function __fish_gleam_deps_direct
     # 'gleam deps list' gives all dependencies but 'gleam remove' takes only
-    # Direct project dependencies listed in 'gleam.toml'
+    # direct project dependencies listed in 'gleam.toml'
     set -l gleam_version (gleam -V | string split -f2 " " | string split ".")
     if test $gleam_version[1] -eq 1 -a $gleam_version[2] -ge 8
         __fish_gleam_deps_parse_tree
@@ -53,9 +52,9 @@ function __fish_gleam_deps_parse_toml
         return 1
     end
     set -l current_table ''
-    # Does not properly escape "#" inside a string but it should not matter
-    # Since we only care about the keys (packages' names) and tables' names
-    # And those should not contain any "#"
+    # does not properly escape "#" inside a string but it should not matter
+    # since we only care about the keys (packages' names) and tables' names
+    # and those should not contain any "#"
     string replace -r '#.*' '' <$project_root/gleam.toml | string trim \
     | while read -l line
         if string match -q '[*]' $line
@@ -93,6 +92,7 @@ complete -c gleam -n __fish_use_subcommand -a build   -d "Build the project"
 complete -c gleam -n __fish_use_subcommand -a check   -d "Type check the project"
 complete -c gleam -n __fish_use_subcommand -a clean   -d "Clean build artifacts"
 complete -c gleam -n __fish_use_subcommand -a deps    -d "Work with dependency packages"
+complete -c gleam -n __fish_use_subcommand -a dev     -d "Run the project development entrypoint"
 complete -c gleam -n __fish_use_subcommand -a docs    -d "Render HTML documentation"
 complete -c gleam -n __fish_use_subcommand -a export  -d "Export from the Gleam project"
 complete -c gleam -n __fish_use_subcommand -a fix     -d "Rewrite deprecated Gleam code"
@@ -117,9 +117,9 @@ complete -c gleam -n '__fish_seen_subcommand_from docs; and __fish_prev_arg_in h
 complete -c gleam -n '__fish_seen_subcommand_from export; and __fish_prev_arg_in help' -a "erlang-shipment hex-tarball javascript-prelude typescript-prelude package-interface package-information"
 complete -c gleam -n '__fish_seen_subcommand_from hex; and __fish_prev_arg_in help' -a "retire unretire revert authenticate"
 
-# Compile: build check run test
-complete -c gleam -n '__fish_seen_subcommand_from build check run test' -s t -l target -rf -a "$targets" -d "The platform to target"
-complete -c gleam -n '__fish_seen_subcommand_from run test' -l runtime -rf -a "$runtimes" -d "The runtime to target"
+# Compile: build check run test dev
+complete -c gleam -n '__fish_seen_subcommand_from build check run test dev' -s t -l target -rf -a "$targets" -d "The platform to target"
+complete -c gleam -n '__fish_seen_subcommand_from run test dev' -l runtime -rf -a "$runtimes" -d "The runtime to target"
 complete -c gleam -n '__fish_seen_subcommand_from build; and not __fish_seen_subcommand_from docs' -l warnings-as-errors -d "Emit compile time warnings as errors"
 complete -c gleam -n '__fish_seen_subcommand_from build run; and not __fish_seen_subcommand_from docs' -l no-print-progress -d "Don't print progress information"
 complete -c gleam -n '__fish_seen_subcommand_from run' -s m -l module -rf -a '(__fish_gleam_runnable_module)' -d "The module to run"
@@ -147,7 +147,7 @@ complete -c gleam -n '__fish_seen_subcommand_from revert' -l version -rf
 complete -c gleam -n '__fish_seen_subcommand_from publish; and not __fish_seen_subcommand_from docs' -l replace
 complete -c gleam -n '__fish_seen_subcommand_from publish; and not __fish_seen_subcommand_from docs' -s y -l yes
 
-# Docs
+# docs
 complete -c gleam -n '__fish_prev_arg_in docs' -a build   -d "Render HTML docs locally"
 complete -c gleam -n '__fish_prev_arg_in docs' -a publish -d "Publish HTML docs to HexDocs"
 complete -c gleam -n '__fish_prev_arg_in docs' -a remove  -d "Remove HTML docs from HexDocs"
@@ -155,7 +155,7 @@ complete -c gleam -n '__fish_seen_subcommand_from docs; and __fish_seen_subcomma
 complete -c gleam -n '__fish_seen_subcommand_from docs; and __fish_seen_subcommand_from remove' -l package -rf -a '(__fish_gleam_hex_packages)' -d "The name of the package"
 complete -c gleam -n '__fish_seen_subcommand_from docs; and __fish_seen_subcommand_from remove' -l version -rf -d "The version of the docs to remove"
 
-# Export
+# export
 complete -c gleam -n '__fish_prev_arg_in export' -a erlang-shipment    -d "Precompiled Erlang, suitable for deployment"
 complete -c gleam -n '__fish_prev_arg_in export' -a hex-tarball        -d "A bundled tarball, suitable for publishing to Hex"
 complete -c gleam -n '__fish_prev_arg_in export' -a javascript-prelude -d "The JavaScript prelude module"
@@ -164,13 +164,14 @@ complete -c gleam -n '__fish_prev_arg_in export' -a package-interface  -d "Infor
 complete -c gleam -n '__fish_prev_arg_in export' -a package-information -d "Information on the package (gleam.toml) in JSON format"
 complete -c gleam -n '__fish_prev_arg_in package-interface package-information' -l out -r -F  -d "The path to write the JSON file to"
 
-# Format
+# format
 complete -c gleam -n '__fish_seen_subcommand_from format; and __fish_not_contain_opt stdin' -F
 complete -c gleam -n '__fish_seen_subcommand_from format' -l stdin -d "Read source from STDIN"
 complete -c gleam -n '__fish_seen_subcommand_from format' -l check -d "Check if inputs are formatted without changing them"
 
-# New
+# new
 complete -c gleam -n '__fish_seen_subcommand_from new' -l name -r -d "Name of the project"
 complete -c gleam -n '__fish_seen_subcommand_from new' -l template -rf -a "$targets" -d "The template to use"
 complete -c gleam -n '__fish_seen_subcommand_from new' -l skip-git    -d "Skip git initialization"
 complete -c gleam -n '__fish_seen_subcommand_from new' -l skip-github -d "Skip creation of .github/* files"
+
