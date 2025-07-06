bec7c5fb67b89f1de105136678978487d7cb1eec
pivoshenko <volodymyr.pivoshenko@gmail.com>
2025-07-06 17:24:23 +0100

    feat(fish): remove loadenv

--- dotfiles/.config/fish/functions.fish
+++ dotfiles/.config/fish/functions.fish
@@ -9,26 +9,6 @@ function rgp
     find . -name ".git" -type d | sed 's/\/\.git//' | xargs -P10 -I{} sh -c 'git -C {} fetch --all && git -C {} pull'
 end
 
-# Load `.env` file from a filename passed as an argument
-function loadenv
-    if test (count $argv) -eq 0
-        echo "Usage: loadenv <file>"
-        return 1
-    end
-    for line in (cat $argv[1])
-        # Ignore comments and empty lines
-        if test (string match -qr '^\s*#' -- $line); or test -z "$line"
-            continue
-        end
-        set --local key (string split -m 1 '=' $line)[1]
-        set --local val (string split -m 1 '=' $line)[2]
-        if test -n "$key" -a -n "$val"
-            set -Ux $key $val
-        end
-    end
-    echo "Loaded"
-end
-
 # Reload `fish` configuration
 function fish
   source ~/.config/fish/config.fish
