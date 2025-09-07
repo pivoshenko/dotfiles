d1f36108dfdbecb887d3ac2fb8bde348ac788c52
pivoshenko <volodymyr.pivoshenko@gmail.com>
2025-09-07 19:24:30 +0100

    feat: update Fish config

--- dotfiles/.config/fish/functions.fish
+++ dotfiles/.config/fish/functions.fish
@@ -1,15 +1,21 @@
 # Delete backup files
 function bakclean
-    fd -H -e ".dotdropbak" -t f -x rm
-    fd -H -e ".bak" -t f -x rm
+  fd -H -e ".dotdropbak" -t f -x rm
+  fd -H -e ".bak" -t f -x rm
+  fd -H -e ".backup" -t f -x rm
 end
 
-# Iterate over sub-directories and pull `git` repositories
+# Iterate over sub-directories and pull Git repositories
 function rgp
-    find . -name ".git" -type d | sed 's/\/\.git//' | xargs -P10 -I{} sh -c 'git -C {} fetch --all && git -C {} pull'
+  command find . -name ".git" -type d | sed 's/\/\.git//' | xargs -P10 -I{} sh -c 'git -C {} fetch --all && git -C {} pull'
 end
 
-# Reload `fish` configuration
+# Reload Fish configuration
 function fish
   source ~/.config/fish/config.fish
+end
+
+# Display system information on shell startup
+function fish_greeting
+  fastfetch
 end
--- dotfiles/.config/fish/vimode.fish
+++ dotfiles/.config/fish/vimode.fish
@@ -9,16 +9,9 @@ function fish_user_key_bindings
     fish_vi_key_bindings --no-erase insert
 end
 
-# Emulates `vim`'s cursor shape behavior
 set fish_cursor_default block
-# Set the insert mode cursor to a line
 set fish_cursor_insert line
-# Set the replace mode cursors to an underscore
 set fish_cursor_replace_one underscore
 set fish_cursor_replace underscore
-# Set the external cursor to a line. The external cursor appears when a command is started
-# The cursor shape takes the value of `fish_cursor_default` when `fish_cursor_external` is not specified
 set fish_cursor_external line
-# The following variable can be used to configure cursor shape in
-# Visual mode, but due to `fish_cursor_default`, is redundant here
 set fish_cursor_visual block
