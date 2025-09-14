2431d9e1c6f3843c4a8f32d4fd550e1f932a5145
pivoshenko <volodymyr.pivoshenko@gmail.com>
2025-09-14 11:50:06 +0100

    feat: update Fish config

--- dotfiles/.config/fish/exports.fish
+++ dotfiles/.config/fish/exports.fish
@@ -2,10 +2,10 @@ set -Ux XDG_CONFIG_HOME $HOME/.config
 
 set -Ux EDITOR hx
 set -Ux VISUAL $EDITOR
+
 set -Ux GPG_TTY (tty)
 
+# == External tools ==
 set -Ux HOMEBREW_NO_ANALYTICS 1
-
 set -Ux PYTHONBREAKPOINT IPython.terminal.debugger.set_trace
-
 set -Ux PATH $PATH $HOME/.spicetify
