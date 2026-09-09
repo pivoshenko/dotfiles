ea78821726962f88c9af2843bc9072eaf55b1f77
pivoshenko <pivoshenko@users.noreply.github.com>
2026-09-09 21:42:23 +0100

    chore(fish): reindent fish_user_key_bindings to two spaces

--- dotfiles/.config/fish/vimode.fish
+++ dotfiles/.config/fish/vimode.fish
@@ -1,8 +1,8 @@
 function fish_user_key_bindings
-    fish_default_key_bindings -M insert
-    fish_vi_key_bindings --no-erase insert
-    bind -M insert ctrl-f __fzf_edit
-    bind -M default ctrl-f __fzf_edit
+  fish_default_key_bindings -M insert
+  fish_vi_key_bindings --no-erase insert
+  bind -M insert ctrl-f __fzf_edit
+  bind -M default ctrl-f __fzf_edit
 end
 
 set fish_cursor_default block
